122c15853e40e7d7d856f008eb04aaa6005488c3
Adam Skorek <askorek@gmail.com>
2016-03-15 15:14:20 +0100

    All functionality is ready Seems to work OK

--- mcnp_mtal_splitter.py
+++ mcnp_mtal_splitter.py
@@ -2,7 +2,7 @@ import sys
 
 class Tally_Splitter(object):
 
-    def split_file(self, file_data):
+    def split_file(self, file_data, filename):
         assert len(file_data) > 0
         assert type(file_data[0]) == str
 
@@ -42,11 +42,19 @@ class Tally_Splitter(object):
                         list_of_values.append(el.split()[0])
                         list_of_accuracy.append(el.split()[1])
 
-                print tally_name
-                print tally_description
-                print list_of_energies
-                print list_of_values
-                print list_of_accuracy
+
+                # saving all to file
+                new_file_name = filename[:-3] + '_' + tally_name + '.csv'
+                file = open(new_file_name,'w')   # Trying to create a new file or open one
+
+                file.write(tally_name + ';' + tally_description)
+                for k in range(len(list_of_energies)):
+                    new_line = list_of_energies[k] + ';' + list_of_values[k] + ';' + list_of_accuracy[k] + '\n'
+                    file.write(new_line.replace('.',','))
+                last_line = 'TOTAL;' + list_of_values[k+1] + ';' + list_of_accuracy[k+1]
+                file.write(last_line.replace('.',','))
+
+                file.close()
             i += 1
 
 
@@ -59,6 +67,6 @@ if __name__ == "__main__":
     with open(filename, 'r') as f:
         file_data = f.readlines()
         ts = Tally_Splitter()
-        ts.split_file(file_data)
+        ts.split_file(file_data, filename)
 
 
